89e1644f730c668785e28da824320fa17c57abde
Murilo <murilo_no@outlook.com>
2024-11-02 15:38:38 -0300

    icone adicionado

--- src/main/java/vetcalc/calculadoravetor/HelloApplication.java
+++ src/main/java/vetcalc/calculadoravetor/HelloApplication.java
@@ -3,15 +3,19 @@ package vetcalc.calculadoravetor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("vetores.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("/images/icon.png"))));
         stage.setTitle("Calculadora de Vetor");
         stage.setScene(scene);
         stage.show();
