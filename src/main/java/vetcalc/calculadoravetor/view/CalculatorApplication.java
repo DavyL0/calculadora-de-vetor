61f3509dda68ef36fb8d0f204e6cfc78f9dffaf1
Murilo <murilo_no@outlook.com>
2024-11-07 22:51:36 -0300

    pequenos ajustes na interface

--- src/main/java/vetcalc/calculadoravetor/view/CalculatorApplication.java
+++ src/main/java/vetcalc/calculadoravetor/view/CalculatorApplication.java
@@ -9,18 +9,25 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * @author Murilo Nunes, Davy Lopes Oliveira, Hartur Sales, Pedro Henrique, Bruno Martins
+ * @date 31/10/2024
+ * @brief Class Main
+ */
+
 public class CalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vetcalc/calculadoravetor/vetores.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/styles.css")).toExternalForm());
 
         stage.getIcons().add(new Image(Objects.requireNonNull(CalculatorApplication.class.getResourceAsStream("/images/icon.png"))));
         stage.setTitle("Calculadora de Vetor");
         stage.setScene(scene);
         stage.show();
+        System.out.println(stage.getWidth() + " " + stage.getHeight());
     }
 
     public static void main(String[] args) {
