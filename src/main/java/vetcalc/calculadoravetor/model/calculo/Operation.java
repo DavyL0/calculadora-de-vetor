7aa13217b7eb40a16d5ddc98c0169f4566b63ad7
Murilo <murilo_no@outlook.com>
2024-11-10 17:50:14 -0300

    comentarios

--- src/main/java/vetcalc/calculadoravetor/model/calculo/Operation.java
+++ src/main/java/vetcalc/calculadoravetor/model/calculo/Operation.java
@@ -15,9 +15,8 @@ public class Operation extends Numeros {
     private double a = numeros.getNumXA() * numeros.getNumXB();
     double b = (numeros.getNumYA() * numeros.getNumYB());
 
-
     // Dentro da classe Operation
-// Método para calcular o módulo de um vetor
+    // Método para calcular o módulo de um vetor
     public double calcularModulo(Numeros vetor) {
         System.out.println("Modulo foi chamado");
         return Math.sqrt(Math.pow(vetor.getNumXA(), 2) + Math.pow(vetor.getNumYA(), 2) + (vetor.getNumZA() != 0 ? Math.pow(vetor.getNumZA(), 2) : 0));
@@ -41,9 +40,10 @@ public class Operation extends Numeros {
     // Método para calcular o produto vetorial entre dois vetores 3D
     public Numeros produtoVetorial(Numeros vetor1, Numeros vetor2) {
         System.out.println("Produto vetorial pra 2 3d foi chamado");
-        if (vetor1.getNumZA() == Double.NaN || vetor2.getNumZA() == Double.NaN) {
-            throw new IllegalArgumentException("Produto vetorial é definido apenas para vetores 3D.");
-        }
+//        nao tem pq lançar essa exceção pq isso nunca vai acontecer bocó
+//        if (vetor1.getNumZA() == Double.NaN || vetor2.getNumZA() == Double.NaN) {
+//            throw new IllegalArgumentException("Produto vetorial é definido apenas para vetores 3D.");
+//        }
         double x = vetor1.getNumYA() * vetor2.getNumZB() - vetor1.getNumZA() * vetor2.getNumYB();
         double y = vetor1.getNumZA() * vetor2.getNumXB() - vetor1.getNumXA() * vetor2.getNumZB();
         double z = vetor1.getNumXA() * vetor2.getNumYB() - vetor1.getNumYA() * vetor2.getNumXB();
